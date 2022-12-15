b8e27cde2b39d8f40c943617032afb13c0e7349c
Kalvium <program@kalvieducation.com>
2022-12-15 15:33:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class CounterApp extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      counter : 0,
+    }
+  }
+
+  handleDecrease = ()=>{
+    this.setState({
+      counter:this.state.counter-1
+    })
+  }
+
+  handleIncrease = ()=>{
+    this.setState({
+      counter:this.state.counter+1
+    })
+  }
+
+  handleReset = ()=>{
+    this.setState({
+      counter:0
+    })
+  }
+
+
+  render(){
+    return(
+      <div className="counter">
+      <h1>Counter App</h1>
+      <span className="counter__output">{this.state.counter}</span>
+      <div className="btn__container">
+        <button className="control__btn" onClick={this.handleIncrease}>+</button>
+        <button className="control__btn" onClick={this.handleDecrease}>-</button>
+        <button className="reset" onClick={this.handleReset}>Reset</button>
+      </div>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
